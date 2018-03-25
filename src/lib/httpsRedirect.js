930a7a78e3b959ad7f2078c7b6c4739c233477b5
Charlotte Spencer <charlottelaspencer@gmail.com>
2018-03-25 21:31:29 +0100

    chore: es6

--- src/lib/httpsRedirect.js
+++ src/lib/httpsRedirect.js
@@ -1,7 +1,7 @@
 function httpsRedirect(req, res, next) {
   if (process.env.NODE_ENV === "production") {
     if (req.headers["x-forwarded-proto"] != "https") {
-      return res.redirect("https://" + req.headers.host + req.url)
+      return res.redirect(`https://${req.headers.host}${req.url}`)
     } else {
       return next()
     }
